8bc9e7244b74565d340f7b3b99e32429568b92da
Xa <xav54350@gmail.com>
2025-01-31 10:33:30 +0100

    initial commit

---
+++ Portfolio/headers.php
@@ -0,0 +1,13 @@
+<nav id="navbar" class="nav">
+    <ul class="nav-list">
+        <li>
+            <a href="#welcome-section">A propos</a>
+        </li>
+        <li>
+            <a href="works.html">Travaux</a>
+        </li>
+        <li>
+            <a href="#contact">Contact</a>
+        </li>
+    </ul>
+</nav>
